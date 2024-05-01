e856eea9845cce5ef8cc5077a2d22b9460d89830
Aaron Crockett <aaron.crockett.00@gmail.com>
2024-04-30 19:30:08 -0700

    passing generateCssValues tests

--- packages/oxygen-styles/scripts/index.test.ts
+++ packages/oxygen-styles/scripts/index.test.ts
@@ -1,6 +1,52 @@
+// @ts-nocheck
 import { expect, test } from "bun:test";
-import { example } from "./index";
+import { getTwColors } from "./index";
 
-test("example test", () => {
-  expect(example()).toBe("example");
+const colorPrefixes = [
+  "primary",
+  "secondary",
+  "tertiary",
+  "error",
+  "warning",
+  "neutral",
+];
+
+const colorSuffixes = [
+  "-50",
+  "-100",
+  "-200",
+  "-300",
+  "-400",
+  "-600",
+  "-700",
+  "-800",
+  "-900",
+  "-950",
+];
+
+const result = getTwColors();
+
+test("getTwColors returns a non-empty object", () => {
+  expect(result).toBeInstanceOf(Object);
+  expect(Object.keys(result)).not.toHaveLength(0);
+});
+
+test("getTwColors returns the correct color keys", () => {
+  colorPrefixes.forEach((prefix) => {
+    expect(Object.keys(result).some((key) => key.startsWith(prefix))).toBe(
+      true
+    );
+  });
+});
+
+test("getTwColors returns the correct color suffixes", () => {
+  colorSuffixes.forEach((suffix) => {
+    expect(Object.keys(result).some((key) => key.endsWith(suffix))).toBe(true);
+  });
+});
+
+test("getTwColors returns keys that end with the prefixes, because the middle value does not include -500", () => {
+  colorPrefixes.forEach((prefix) => {
+    expect(Object.keys(result).some((key) => key.endsWith(prefix))).toBe(true);
+  });
 });
--- packages/oxygen-styles/scripts/index.ts
+++ packages/oxygen-styles/scripts/index.ts
@@ -205,7 +205,3 @@ export const getTwColors = () => {
 
   return results;
 };
-
-export function example() {
-  return "example";
-}
